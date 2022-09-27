caeea74c919335fc65aa175137db5b8b67f3c7ae
Jaeee <wldms1107@gmail.com>
2022-09-27 20:48:05 +0900

    경로 수정

--- server/index.js
+++ server/index.js
@@ -98,7 +98,7 @@ app.listen(port, () => {
 
 //정적 파일 제공
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "/../client/build")));
 }
 
 app.get("*", (req, res) => {
