9de63909e114b60eadd8d94205e8d72497941218
Jaeee <wldms1107@gmail.com>
2022-09-27 16:44:04 +0900

    정적 파일 추가

--- server/index.js
+++ server/index.js
@@ -98,6 +98,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// 리액트 정적 파일 제공
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("*", (req, res) => {
   // eslint-disable-next-line no-undef
   res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
